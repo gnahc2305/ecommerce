f2a52bed751534283f989050b72b785d76086533
gnahc2305 <achangmurillo@gmail.com>
2022-12-21 14:53:02 -0500

    add styles

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import React from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      hola
+      HeroBanner
+
+      <div>
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+
+      <div>
+        {['Product1', 'Product2'].map((product) => product)}
+      </div>
+
+      Footer
     </>
-  )
+  );
 }
