8d8430ea617bf8f17d815a03a84c3b47b1404dfa
gnahc2305 <achangmurillo@gmail.com>
2022-12-21 14:59:43 -0500

    create files and structure

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
 import React from "react";
+
+import { Product, FooterBanner, HeroBanner} from '../components/index';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
 
-      <div>
+      <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
-      <div>
+      <div className="products-container">
         {['Product1', 'Product2'].map((product) => product)}
       </div>
 
-      Footer
+      <FooterBanner />
     </>
   );
 }
