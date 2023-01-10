ab520678ae9dc868d083fbe8b4aa543c02e92cc3
gnahc2305 <achangmurillo@gmail.com>
2023-01-09 20:20:17 -0500

    products are now displayed

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
-function Product({ product }) {
+import { urlFor } from '../LIB/client';
+
+function Product({ product: { image, name, slug, price } }) {
   return (
     <div>
-      product
+      <Link href={`/product/${slug.current}`}>
+        <div className='product-card'> 
+          <img src={urlFor(image && image[0])} alt="product" width={250} height={250} className='product-image'/>
+          <p className='product-name'>{name}</p>
+          <p className='product-price'>${price}</p>
+        </div>
+      </Link>
     </div>
   )
 }
